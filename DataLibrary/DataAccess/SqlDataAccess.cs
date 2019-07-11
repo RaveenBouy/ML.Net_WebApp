e8d9c36ea79c3ef3b765e127e4edd28854d9743e
RavianXReaver <raveenshenal@gmail.com>
2019-07-11 14:10:17 +0530

    Project Initiation.

---
+++ DataLibrary/DataAccess/SqlDataAccess.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLibrary.DataAccess
+{
+    public class SqlDataAccess
+    {
+        private static string GetConnectionString()
+        {
+            string conString = "Server=127.0.0.1; Port=3306; Database=APIProject; Uid=root";
+
+            return conString;
+        }
+
+        public static List<T> LoadData<T>(string sql)
+        {
+
+        }
+    }
+}
